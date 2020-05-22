efaac3599f0c683a6919f8db3bce0c8dbc78da21
Jared Forth <jaredforthdev@gmail.com>
2020-05-22 15:11:45 -0500

    update setter functions

--- src/notation/rhythm.rs
+++ src/notation/rhythm.rs
@@ -62,4 +62,52 @@ impl NoteDuration {
             duration_type: Default::default()
         }
     }
+    /// Set length for a duration
+    ///
+    /// # Usage:
+    ///
+    /// ```
+    ///
+    /// use lilypond::notation::rhythm::{NoteDuration, Length};
+    ///
+    /// let mut  duration = NoteDuration::new();
+    /// duration.length(Length::Sixteenth);
+    ///
+    /// assert_eq!(Length::Sixteenth, duration.length)
+    /// ```
+    pub fn length(&mut self, length: Length) {
+        self.length = length;
+    }
+    /// Set whether or not a duration is dotted
+    ///
+    /// # Usage:
+    ///
+    /// ```
+    ///
+    /// use lilypond::notation::rhythm::NoteDuration;
+    ///
+    /// let mut  duration = NoteDuration::new();
+    /// duration.dotted(true);
+    ///
+    /// assert_eq!(true, duration.dotted)
+    /// ```
+    pub fn dotted(&mut self, is_dotted: bool) {
+        self.dotted = is_dotted;
+    }
+    /// Set duration type
+    ///
+    /// # Usage:
+    ///
+    /// ```
+    ///
+    /// use lilypond::notation::rhythm::{NoteDuration, NoteDurationType};
+    ///
+    /// let mut  duration = NoteDuration::new();
+    /// duration.duration_type(NoteDurationType::Rest);
+    ///
+    /// assert_eq!(NoteDurationType::Rest, duration.duration_type)
+    /// ```
+    pub fn duration_type(&mut self, duration_type: NoteDurationType) {
+        self.duration_type = duration_type;
+    }
 }
