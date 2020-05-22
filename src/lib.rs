b3cf7db7d73eb9eefed01e3057ba90ad01f4d6cf
Jared Forth <jaredforthdev@gmail.com>
2020-05-22 11:14:21 -0500

    update docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+// Copyright 2020 Jared Forth.
+//
+// Licensed under the GNU General Public License v3.0 <LICENSE or
+// https://www.gnu.org/licenses/gpl-3.0.en.html>.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Utilities for common filesystem operations.
+//!
+//! **lilypond** provides an API to ergonomically wrap LilyPond,
+//! and provide Rust types that resolve to LilyPond output.
+
 pub fn lilypond() {
 
 }
